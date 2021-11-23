7b25b09ad24eb5009bc88d6b7b5b157e81f0dab5
Vivek <itguru4all@gmail.com>
2021-11-23 11:21:14 +0530

    Added Database Test

---
+++ app/src/main/java/com/udacity/asteroidradar/database/RoomDatabase.kt
@@ -0,0 +1,68 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+@Entity(tableName = "asteroid_table")
+data class AsteroidEntity(
+                        @PrimaryKey
+                          val id: Long,
+                          val codename: String,
+                          val closeApproachDate: String,
+                          val absoluteMagnitude: Double,
+                          val estimatedDiameter: Double,
+                          val relativeVelocity: Double,
+                          val distanceFromEarth: Double,
+                          val isPotentiallyHazardous: Boolean) : Parcelable
+
+@Dao
+interface AsteroidDao{
+@Query("Select * from asteroid_table Order by closeApproachDate")
+fun get() : List<AsteroidEntity>?
+
+  @Insert
+   fun insert(asteroid : AsteroidEntity)
+
+  @Query("DELETE FROM asteroid_table ")
+  fun clear()
+}
+
+@Database(entities = [AsteroidEntity::class],version = 1,exportSchema = false)
+abstract class AsteroidDatabase : RoomDatabase(){
+
+  abstract val asteroidDao : AsteroidDao
+
+  companion object{
+
+    @Volatile
+    private var INSTANCE : AsteroidDatabase? = null
+
+    fun getInstance(context: Context) : AsteroidDatabase{
+    synchronized(this)
+    {
+      var instance = INSTANCE
+
+      if(instance == null)
+      {
+        instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AsteroidDatabase::class.java,
+                  "asteroid_history_database")
+          .fallbackToDestructiveMigration()
+          .build()
+        INSTANCE = instance
+      }
+
+
+      return instance
+    }
+
+
+    }
+  }
+}
